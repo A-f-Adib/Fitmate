4fefb70362f3e4a71df8d4c08244233392bc9679
A.F. Adib <a.f.adib36@gmail.com>
2025-01-10 02:45:20 +0600

    modified

--- Fitmate/Views/FitnessTabView.swift
+++ Fitmate/Views/FitnessTabView.swift
@@ -11,18 +11,29 @@ struct FitnessTabView: View {
     
     @State var selectedTab = "Home"
     
+    init() {
+        let appearance = UITabBarAppearance()
+        appearance.configureWithOpaqueBackground()
+        appearance.stackedLayoutAppearance.selected.iconColor = .green
+        
+        UITabBar.appearance().scrollEdgeAppearance = appearance
+    }
+    
+    
     var body: some View {
         TabView(selection: $selectedTab) {
            HomeView()
                 .tag("Home")
                 .tabItem {
                     Image(systemName: "house")
+                    Text("Home")
                 }
             
             HistoricDataView()
                 .tag("Historic")
                 .tabItem {
                     Image(systemName: "chart.line.uptrend.xyaxix")
+                    Text("Charts")
                 }
         }
     }
