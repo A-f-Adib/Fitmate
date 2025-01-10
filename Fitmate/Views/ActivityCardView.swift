02bec2a0dd00a1f4e916483320af1a555d60d274
A.F. Adib <a.f.adib36@gmail.com>
2025-01-11 02:51:59 +0600

    activity card view

--- Fitmate/Views/ActivityCardView.swift
+++ Fitmate/Views/ActivityCardView.swift
@@ -10,7 +10,31 @@ import SwiftUI
 struct ActivityCardView: View {
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            
+        ZStack {
+            Color(uiColor: .systemGray6)
+                .cornerRadius(15)
+            
+            VStack {
+                HStack(alignment: .top) {
+                    VStack(alignment: .leading, spacing: 8) {
+                        Text("Todays Steps")
+                        Text("Goal 10,000")
+                    }
+                    
+                    Spacer()
+                    
+                    Image(systemName: "figure.walk")
+                        .foregroundColor(.green)
+                }
+                
+                Text("6,121")
+                    .font(.title)
+                    .bold()
+                    .padding()
+            }
+            .padding()
+        }
     }
 }
 
