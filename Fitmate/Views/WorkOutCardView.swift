dfd6013751d70986a44ebdf22023b375cd88024f
A.F. Adib <a.f.adib36@gmail.com>
2025-01-11 03:37:36 +0600

    workout card view

---
+++ Fitmate/Views/WorkOutCardView.swift
@@ -0,0 +1,60 @@
+//
+//  WorkOutCardView.swift
+//  Fitmate
+//
+//  Created by A.F. Adib on 11/1/25.
+//
+
+import SwiftUI
+
+struct Workout {
+    let id: Int
+    let title: String
+    let image: String
+    let tintColor: Color
+    let duration: String
+    let date : String
+    let calories: String
+}
+
+struct WorkOutCardView: View {
+    
+    @State var workout : Workout
+    
+    var body: some View {
+        
+        HStack{
+            Image(systemName: workout.image)
+                .resizable()
+                .scaledToFit()
+                .frame(width: 48, height: 48)
+                .foregroundColor(.green)
+                .padding()
+                .background(.gray.opacity(0.1))
+                .cornerRadius(10)
+            
+            VStack(spacing: 16) {
+                HStack {
+                    Text(workout.title)
+                        .font(.title3)
+                        .bold()
+                    Spacer()
+                    
+                    Text(workout.duration)
+                }
+                HStack {
+                    Text(workout.date)
+                    
+                    Spacer()
+                    
+                    Text(workout.calories)
+                }
+            }
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    WorkOutCardView(workout: Workout(id: 0, title: "Running", image: "figure.walk", tintColor: .cyan, duration: "51 mins", date: "Sept 2", calories: "512 KCal"))
+}
