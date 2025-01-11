4475ec7caf1f423a2fb366246b0e28afb2d7c1b2
A.F. Adib <a.f.adib36@gmail.com>
2025-01-12 02:58:49 +0600

    fetch exercise time

--- Fitmate/Manager/HealthManager.swift
+++ Fitmate/Manager/HealthManager.swift
@@ -33,6 +33,34 @@ class HealthManager {
     func fetchTodayCaloriesBurned(completion: @escaping(Result<Double, Error>) -> Void) {
         let calories = HKQuantityType(.activeEnergyBurned)
         let predicate = HKQuery.predicateForSamples(withStart: .startOfDay, end: Date())
+        
+        let query = HKStatisticsQuery(quantityType: calories, quantitySamplePredicate: predicate) { _, results, error in
+            guard let quantity = results?.sumQuantity(), error == nil else {
+                completion(.failure(NSError()))
+                return
+            }
+            
+            let caloriCount = quantity.doubleValue(for: .kilocalorie())
+            completion(.success(caloriCount))
+        }
+        healthStore.execute(query)
+    }
+    
+    
+    func fetchTodayExerciseTime(completion: @escaping(Result<Double, Error>) -> Void) {
+        let exercise = HKQuantityType(.appleExerciseTime)
+        let predicate = HKQuery.predicateForSamples(withStart: .startOfDay, end: Date())
+        
+        let query = HKStatisticsQuery(quantityType: exercise, quantitySamplePredicate: predicate) { _, results, error in
+            guard let quantity = results?.sumQuantity(), error == nil else {
+                completion(.failure(NSError()))
+                return
+            }
+            
+            let exerciseTime = quantity.doubleValue(for: .minute())
+            completion(.success(exerciseTime))
+        }
+        healthStore.execute(query)
     }
 }
 
